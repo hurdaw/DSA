84efa7d34a50ec5498db5085839e5ee6f35239e1
hurdaw <tn133931@gmail.com>
2024-03-16 00:12:51 +0700

    SLL_easier

---
+++ SLL_new.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+// khai báo SLL
+struct node
+{
+    int data;
+    struct node *pNext;
+};
+typedef struct node NODE;
+struct list
+{
+    NODE *pHead;
+    NODE *pTail;
+};
+typedef struct list LIST;
+// khởi tạo
+void init(LIST &l)
+{
+    l.pHead = l.pTail = NULL;
+}
+NODE *create_node(int value)
+{
+    NODE *temp = new (NODE);
+    if (temp == NULL)
+        return NULL;
+    temp->data = value;
+    temp->pNext = NULL;
+    return temp;
+}
+// thêm node p vào đầu danh sách
+void insert_begin(LIST &l, NODE *p)
+{
+    if (l.pHead == NULL)
+        l.pHead = l.pTail = p;
+    else
+    {
+        p->pNext = l.pHead; // p quăng dây tham gia vào danh sách
+        l.pHead = p;
+    }
+}
+void insert_last(LIST &l, NODE *p)
+{
+    if (l.pHead == NULL)
+    {
+        l.pHead = l.pTail = p;
+    }
+    else
+    {
+        l.pTail->pNext = p;
+        l.pTail = p;
+    }
+}
+void input(list &l)
+{
+    int n;
+    cout << "nhap so node: ";
+    cin >> n;
+    init(l);
+    for (int i = 1; i <= n; i++)
+    {
+        int x;
+        cout << endl
+             << "Gia tri cua node la: ";
+        cin >> x;
+        NODE *p = create_node(x);
+        insert_last(l, p);
+    }
+}
+void output(LIST l)
+{
+    for (NODE *p = l.pHead; p != NULL; p = p->pNext)
+    {
+        cout << p->data << " ";
+    }
+}
+int max(int a[], int n)
+{
+    int max = a[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > max)
+            max = a[i];
+    }
+    return max;
+}
+int max(LIST l)
+{
+    // int max=a[0];
+    int max = l.pHead->data;
+    for (NODE *p = l.pHead->pNext; p != NULL; p = p->pNext)
+    {
+        if (p->data > max)
+            max = p->data;
+    }
+    return max;
+}
+int main()
+{
+    LIST l;
+    input(l);
+    output(l);
+    cout << endl;
+    cout << max(l);
+}
--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,90 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+struct node {
+    float soMu;
+    float heSo;
+    node *next;
+};
+
+class single_linked_list {
+public:
+    node *start;
+    single_linked_list() {
+        start = NULL;
+    }
+    node *create_node(float, float);
+    void insert_begin();
+    void insert_last();
+    void display();
+};
+
+node *single_linked_list::create_node(float heSo, float soMu) {
+    node *p = new node;
+    if (p == nullptr) {
+        cout << "Khong du bo nho cap phat" << endl;
+        return nullptr;
+    } else {
+        p->heSo = heSo;
+        p->soMu = soMu;
+        p->next = NULL;
+        return p;
+    }
+}
+
+void single_linked_list::insert_begin() {
+    float heSo, soMu;
+    cout << "Nhap vao he so: ";
+    cin >> heSo;
+    cout << "Nhap vao so mu: ";
+    cin >> soMu;
+    node *temp = create_node(heSo, soMu);
+    if (temp == nullptr) return; // Kiểm tra trường hợp cấp phát không thành công
+    if (start == NULL) {
+        start = temp;
+    } else {
+        temp->next = start;
+        start = temp;
+    }
+}
+
+void single_linked_list::insert_last() {
+    float heSo, soMu;
+    cout << "Nhap vao he so: ";
+    cin >> heSo;
+    cout << "Nhap vao so mu: ";
+    cin >> soMu;
+    node *temp = create_node(heSo, soMu);
+    if (temp == nullptr) return; // Kiểm tra trường hợp cấp phát không thành công
+    if (start == NULL) {
+        start = temp;
+    } else {
+        node *p = start;
+        while (p->next != NULL) {
+            p = p->next;
+        }
+        p->next = temp;
+    }
+}
+
+void single_linked_list::display() {
+    node *temp = start;
+    while (temp != NULL) {
+        cout << temp->heSo << "x^" << temp->soMu;
+        if (temp->next != NULL) cout << " + ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    single_linked_list sll;
+
+    // Ví dụ: thêm 5 nút
+    for (int i = 0; i < 5; ++i) {
+        sll.insert_last();
+    }
+
+    sll.display();
+    return 0;
 }
